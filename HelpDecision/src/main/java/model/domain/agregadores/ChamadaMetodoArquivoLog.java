1fc4cf82ca3d4a5a1101aa3b4fde2bc9b4f32d74
gdaneluti <gustavo_daneluti@hotmail.com>
2016-11-04 11:43:28 -0200

    Teste Git

--- HelpDecision/src/main/java/model/domain/agregadores/ChamadaMetodoArquivoLog.java
+++ HelpDecision/src/main/java/model/domain/agregadores/ChamadaMetodoArquivoLog.java
@@ -7,7 +7,8 @@ public class ChamadaMetodoArquivoLog {
 
 	private ChamadaMetodo chamadaMetodo;
 	private ArquivoLog arquivoLog;
-
+	String testGit;
+	
 	private ChamadaMetodoArquivoLog(ChamadaMetodo chamadaMetodo, ArquivoLog arquivoLog) {
 		this.chamadaMetodo = chamadaMetodo;
 		this.arquivoLog = arquivoLog;
