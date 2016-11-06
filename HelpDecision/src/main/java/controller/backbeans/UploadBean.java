97facddc5ce70037b255e346d1b90e82578db18b
gdaneluti <gustavo_daneluti@hotmail.com>
2016-11-06 16:53:25 -0200

    Tela de Upload, css e scripts

---
+++ HelpDecision/src/main/java/controller/backbeans/UploadBean.java
@@ -0,0 +1,12 @@
+package controller.backbeans;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class UploadBean {
+
+	public UploadBean() {
+		// TODO Auto-generated constructor stub
+	}
+	
+}
