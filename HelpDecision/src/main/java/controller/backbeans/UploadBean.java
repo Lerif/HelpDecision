230d60e3227719292491eb9d3f53e04e32883e4c
Lerif <ellogador@gmail.com>
2016-11-06 22:04:06 -0200

    Teste de upload funcionando apenas em diretorio home

--- HelpDecision/src/main/java/controller/backbeans/UploadBean.java
+++ HelpDecision/src/main/java/controller/backbeans/UploadBean.java
@@ -1,12 +1,43 @@
 package controller.backbeans;
 
+import java.io.IOException;
+
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.servlet.http.Part;
 
 @ManagedBean
+@SessionScoped
 public class UploadBean {
 
+	private Part arquivo;
+	
 	public UploadBean() {
-		// TODO Auto-generated constructor stub
+		
 	}
+	
+	public void upload() throws IOException{
+		arquivo.write("//home//skannon//" + buscarNomeDoArquivo(arquivo));
+	}
+	
+	public String buscarNomeDoArquivo(Part part){
+		
+		String nomeArquivo;
+		
+		for (String cd : part.getHeader("content-disposition").split(";")){
+			nomeArquivo = cd.substring(cd.indexOf('=') +1).trim().replace("\"", "");
+			return nomeArquivo.substring(nomeArquivo.lastIndexOf('/') +1).substring(nomeArquivo.lastIndexOf('\\') +1);
+		}
+		return null;
+	}
+
+	public Part getArquivo() {
+		return arquivo;
+	}
+
+	public void setArquivo(Part arquivo) {
+		this.arquivo = arquivo;
+	}
+	
 	
 }
